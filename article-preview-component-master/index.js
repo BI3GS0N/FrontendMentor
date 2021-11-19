6c1c7d4e6fe5b6e882104cc0da5b798b41ac9919
Sylwester Biega <biegson11@gmail.com>
2021-11-19 23:46:40 +0100

    article preview's mobile view added, update readme

---
+++ article-preview-component-master/index.js
@@ -0,0 +1,7 @@
+const shareBtn = document.querySelector('.footer__share-button');
+const share = document.querySelector('.share');
+
+shareBtn.addEventListener('click', () => {
+    shareBtn.classList.toggle('active');
+    share.classList.toggle('active');
+})
